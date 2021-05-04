427583d113f547f3dc3719890253c7f22f4bdf91
diegohfcelestino <diegohfcelestino@gmail.com>
2021-05-04 13:33:08 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -21,73 +21,73 @@ const DataTable = () => {
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Caitlin Snow</td>
+                        <td>30</td>
+                        <td>20</td>
+                        <td>14017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Cisco Ramon</td>
+                        <td>31</td>
+                        <td>20</td>
+                        <td>19017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Harrison Wells</td>
+                        <td>10</td>
+                        <td>8</td>
+                        <td>5017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Joe West</td>
+                        <td>23</td>
+                        <td>18</td>
+                        <td>11017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Iris</td>
+                        <td>35</td>
+                        <td>26</td>
+                        <td>16017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Oliver Queen</td>
+                        <td>20</td>
+                        <td>10</td>
+                        <td>9017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>John Diggle</td>
+                        <td>5</td>
+                        <td>4</td>
+                        <td>3017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Felicity Smoak</td>
+                        <td>40</td>
+                        <td>33</td>
+                        <td>25017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Sara Lance</td>
+                        <td>23</td>
+                        <td>17</td>
+                        <td>7017.00</td>
                     </tr>
                     <tr>
                         <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
+                        <td>Roy Harper</td>
+                        <td>19</td>
+                        <td>12</td>
+                        <td>8117.00</td>
                     </tr>
                 </tbody>
             </table>
