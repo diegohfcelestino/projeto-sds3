de0d6fb4a252370ffb8e69c8df9f29d10d383a2e
diegohfcelestino <diegohfcelestino@gmail.com>
2021-05-08 00:43:46 -0300

    DataTable integration

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,4 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SalePage } from "types/sale";
+import { formatLocalDate } from "utils/format";
+import { BASE_URL } from "utils/request";
+
 const DataTable = () => {
+
+    const [page, setPage] = useState<SalePage>({
+        first: true,
+        last: true,
+        number: 0,
+        totalElements: 0,
+        totalPages: 0,
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales?page=5&size=10&sort=date,desc`)
+            .then(response => {
+                setPage(response.data);
+            });
+    }, [])
+
     return (
         <div className="table-responsive">
             <table className="table table-striped table-sm">
@@ -12,83 +34,16 @@ const DataTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Barry Allen</td>
-                        <td>34</td>
-                        <td>25</td>
-                        <td>15017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Caitlin Snow</td>
-                        <td>30</td>
-                        <td>20</td>
-                        <td>14017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Cisco Ramon</td>
-                        <td>31</td>
-                        <td>20</td>
-                        <td>19017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Harrison Wells</td>
-                        <td>10</td>
-                        <td>8</td>
-                        <td>5017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Joe West</td>
-                        <td>23</td>
-                        <td>18</td>
-                        <td>11017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Iris</td>
-                        <td>35</td>
-                        <td>26</td>
-                        <td>16017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Oliver Queen</td>
-                        <td>20</td>
-                        <td>10</td>
-                        <td>9017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>John Diggle</td>
-                        <td>5</td>
-                        <td>4</td>
-                        <td>3017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Felicity Smoak</td>
-                        <td>40</td>
-                        <td>33</td>
-                        <td>25017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Sara Lance</td>
-                        <td>23</td>
-                        <td>17</td>
-                        <td>7017.00</td>
-                    </tr>
-                    <tr>
-                        <td>22/04/2021</td>
-                        <td>Roy Harper</td>
-                        <td>19</td>
-                        <td>12</td>
-                        <td>8117.00</td>
-                    </tr>
+                    {page.content?.map(item => (
+                        <tr key={item.id}>
+                            <td>{formatLocalDate(item.date, "dd/MM/yyyy")}</td>
+                            <td>{item.seller.name}</td>
+                            <td>{item.visited}</td>
+                            <td>{item.deals}</td>
+                            <td>{item.amount.toFixed(2)}</td>
+                        </tr>
+                    ))
+                    }
                 </tbody>
             </table>
         </div>
